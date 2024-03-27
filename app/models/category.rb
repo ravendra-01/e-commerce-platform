5b919c94211c29033a4c889d08a440683a728e38
rkpatel <rk.ravendra01@gmail.com>
2024-03-27 18:20:57 +0530

    added admin pages to manage category wise offers

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,6 +2,7 @@ class Category < ApplicationRecord
   has_many :categories, class_name: "Category",
                           foreign_key: "parent_category_id"
   has_many :options
+  has_many :category_offers
 
   belongs_to :parent_category, class_name: "Category", optional: true
   validates :category_name, presence: true, uniqueness: true
